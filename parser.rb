539c67789775b9b7cda8e3cd9859f3ed0119ebd7
Parker Odrich <podrich@stanford.edu>
2015-03-26 14:14:13 -0400

    Solved the line and character counts, currently working on word count

---
+++ parser.rb
@@ -0,0 +1,17 @@
+lines=0
+words=0
+characters=0
+constitution='constitution.txt'
+File.open(constitution).each do |line|
+	lines+=1
+    line.each_char do |character|
+    	if character==""||character==':'||character=='.'||character==";"||character==","
+    		words+=1
+    	end
+    	characters+=1
+    end
+end
+
+puts lines
+puts words
+puts characters
