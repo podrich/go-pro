7d5befa0e202117cd79086d710d505aa3ccfbb78
Parker Odrich <podrich@stanford.edu>
2015-03-28 22:23:38 -0400

    Article counting resolved

--- parser.rb
+++ parser.rb
@@ -6,23 +6,22 @@ nonProperWords=0
 nonProperCharacters=0
 constitution='constitution.txt'
 articles=0
+previous=" "
 File.open(constitution).each do |line|
 	lines+=1
 	words+=line.scan(/\S+/).size
     line.each_char do |character|
     	characters+=1
     end
+    candidates=line.scan(/\S+/)
+    if candidates[0]=="Article"&&previous.size==0
+    	articles+=1
+    end
     line=line.upcase
 	candidates=line.scan(/\S+/)
-	isLineProper=false
-	counter=false	
+	isLineProper=false	
 	candidates.each do |choice|
 		choice.gsub(/[^A-Za-z0-9\s]/i, '')
-		if choice=="ARTICLE"&&counter==false
-			puts choice
-			articles+=1
-			counter=true
-		end
 		if choice=="I"||choice=="WE"||choice=="THEY"||choice=="A"||choice=="AND"||choice=="THE"||choice=="THAT"||choice=="OF"||choice=="FOR"||choice=="WITH"
 			nonProperWords+=1
 			if isLineProper==false
@@ -32,7 +31,9 @@ File.open(constitution).each do |line|
 			nonProperCharacters+=choice.length
 		end
 	end	
+	previous=candidates
 end
+puts articles 
 properLines=lines - nonProperLines
 properWords=words - nonProperWords
 properCharacters=characters - nonProperCharacters
@@ -44,4 +45,3 @@ puts "proper: #{properLines}  #{properWords}  #{properCharacters}"
 puts "Total Articles: "
 puts "Total Sections: "
 puts "Total Sections per Article:"
-puts articles 
